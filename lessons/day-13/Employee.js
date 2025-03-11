bba4af8226fef269c34cd95ca65eaee2bfed2d56
uyen-nguyenthuy-tpv-clv <uyen.nguyenthuy.tpv@one-line.com>
2025-03-11 19:36:21 +0700

    Lesson day 16,17

---
+++ lessons/day-13/Employee.js
@@ -0,0 +1,24 @@
+class Employee {
+  constructor(name, salary) {
+    this._name = name;
+    this._salary = salary;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get salary() {
+    return this.salary;
+  }
+
+  set salary(salary) {
+    this.salary = salary;
+  }
+
+}
+
